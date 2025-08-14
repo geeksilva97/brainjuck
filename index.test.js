5d200f04e9d4634813cfc77f07c1c7dee849c097
Edy Silva <edigleyssonsilva@gmail.com>
2025-08-13 22:33:03 -0300

    test parser

--- index.test.js
+++ index.test.js
@@ -1,10 +1,11 @@
 import test, { describe, it } from 'node:test';
 import { parseBrainfuck } from './index.js';
-import assert from 'node:assert';
 
 
-test.only('brainfuck parsing', () => {
+test('brainfuck parsing', (t) => {
   const tokens = parseBrainfuck(`
+  ,
+  .
   ++
   >
   +
@@ -16,5 +17,19 @@ test.only('brainfuck parsing', () => {
   ]
   `);
 
-  console.log({tokens})
+
+  t.assert.deepStrictEqual(tokens, [
+    { type: 'input' },
+    { type: 'output' },
+    { type: 'increment', inc: 2 },
+    { type: 'move_head', head: 1 },
+    { type: 'increment', inc: 1 },
+    { type: 'jump_eqz', jmp: 11 },
+    { type: 'move_head', head: 0 },
+    { type: 'increment', inc: 1 },
+    { type: 'move_head', head: 1 },
+    { type: 'increment', inc: -1 },
+    { type: 'jump_neqz', jmp: 6 },
+    { type: 'halt' }
+  ])
 });
